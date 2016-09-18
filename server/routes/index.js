d2ab62e21d83dfefe79aa6f877874fb366981811
scyclow <spike716@gmail.com>
2016-09-18 16:35:20 -0400

    add procfile

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const user = require('./user');
+const User = require('../models/user');
 
 module.exports = (app) => {
   app.use('/api/user', user);
 
   app.get('/', (req, res) => {
-    res.send('This is the root!');
+    User.findOne({})
+      .then(user => res.send(user))
+      .catch(err => res.send(err))
   });
 };
