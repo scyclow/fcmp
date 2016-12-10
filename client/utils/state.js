74cfeb02c9ebc5719ebfdbd67575eaa9f3308563
scyclow <spike716@gmail.com>
2016-12-10 18:36:23 -0500

    state module foundation

---
+++ client/utils/state.js
@@ -0,0 +1,33 @@
+// @flow weak
+'use strict';
+
+const _ = require('./_');
+const STATE = {};
+const actionsHandlers = [];
+
+
+
+
+
+
+/*
+
+state.event({
+  type: 'something',
+  data: 10
+})
+
+state.register(
+  'substate',
+  { data: 5 },
+  {
+    something: (action, state) => state.something.data = action.data,
+    somethingElse: (action, state) => state.something.data = action.data,
+  }
+);
+
+state.onChange('substate', (substate) => {
+  component.innerHTML = substate.data
+})
+
+*/
